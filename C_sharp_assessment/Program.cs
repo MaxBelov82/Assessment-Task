138ed446383c01439a7515f04d89c44f00c17f55
Maxim Belov <belov.max7@yandex.ru>
2022-08-22 21:13:45 +0300

    добавил файл Readme

---
+++ C_sharp_assessment/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу,которая из имеющегося массива строк 
+// формирует массив из строк, длина которых меньше
+// либо равна 3 символа. Первоначальный массив можно ввести с 
+// клавиатуры, либо задать на старте исключительно массивами.
+
+// // ["hello", "2","world", ":-)"] -> ["2", ":-)"]
+
+string[] StringArray1 = new string[5];
+string [] StringArray2 = new string[StringArray1.Length];
+void SecondArray(string[] StringArray1, string[] StringArray2)
+{
+    int count =0 ;
+for (int i = 0; i < StringArray1.Length; i++)
+{
+    if (StringArray1.Length <= 3)
+    {
+      StringArray2[count] = StringArray1[i];
+      count++;
+      }
+   }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(StringArray1, StringArray2);
+PrintArray(StringArray2);
