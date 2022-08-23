f532493b67884514273542edb743c0fdc050f108
Maxim Belov <belov.max7@yandex.ru>
2022-08-23 21:06:10 +0300

    задачу сделал

--- C_sharp_assessment/Program.cs
+++ C_sharp_assessment/Program.cs
@@ -3,29 +3,33 @@
 // либо равна 3 символа. Первоначальный массив можно ввести с 
 // клавиатуры, либо задать на старте исключительно массивами.
 
-// // ["hello", "2","world", ":-)"] -> ["2", ":-)"]
+// ["hello", "2","world", ":-)"] -> ["2", ":-)"]
 
-string[] StringArray1 = new string[5];
-string [] StringArray2 = new string[StringArray1.Length];
+string[] StringArray1 = new string[] { "hello", "2", "world", ":-)" };
+string[] StringArray2 = new string[StringArray1.Length];
 void SecondArray(string[] StringArray1, string[] StringArray2)
 {
-    int count =0 ;
-for (int i = 0; i < StringArray1.Length; i++)
-{
-    if (StringArray1.Length <= 3)
+    int count = 0;
+    for (int i = 0; i < StringArray1.Length; i++)
     {
-      StringArray2[count] = StringArray1[i];
-      count++;
-      }
-   }
+        if (StringArray1[i].Length <= 3)
+        {
+            StringArray2[count] = StringArray1[i];
+            count++;
+        }
+    }
 }
 void PrintArray(string[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
+        {   
+           if( i==0)
+             Console.Write(array[i] + ","); 
+             else   
+                Console.Write(array[i]);
     }
-    Console.WriteLine();
+    Console.Write("]");
 }
 SecondArray(StringArray1, StringArray2);
 PrintArray(StringArray2);
